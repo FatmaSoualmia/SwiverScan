780c1743b4c3c136bf75b626580663ca0be3b279
FatmaSoualmia <121440223+FatmaSoualmia@users.noreply.github.com>
2023-09-25 09:22:44 +0100

    Update NotFound.js

--- src /pages/NotFound.js
+++ src /pages/NotFound.js
@@ -1,12 +1,12 @@
 import { NavLink } from 'react-router-dom'
-import ErrorPage from './ErrorPage.jpg' 
+import ErrorPage from '../assets/ErrorPage.jpg' 
 
 function NotFound() {
   return (
     <div>
      <h1>Page not found</h1>
      <h3>Go to <NavLink to="/">Home</NavLink> page</h3>
-     <img className='w-75' src={ErrorPage} alt="error page" />
+     <img className='w-50' src={ErrorPage} alt="error page" />
      
     </div>
   )
